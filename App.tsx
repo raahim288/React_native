9bb0719052dc392ad6f906ba451d39b23942b9f7
Abdul Raahim <66337475+raahim288@users.noreply.github.com>
2024-09-28 13:58:47 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function () {
+  return (
+    <View>
+      <Text>
+      Hello world 
+      </Text>
+    </View>
+  )
+}
